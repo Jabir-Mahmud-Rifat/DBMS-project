cedae988c6305c664d5043449857243cff4112f2
Arjun <arjunkarmaker03@gmail.com>
2022-12-07 05:29:03 +0600

    created the basic form

--- login and signup.php
+++ login and signup.php
@@ -1,3 +1,61 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Welcome to Retail Club</title>
 
-?>
+    <script src="https://kit.fontawesome.com/98e008cd61.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+    <div class="container">
+        <div class="forms-container">
+            <div class="login-signup">
+                <form action="" class="sign-in-form">
+                    <h2 class="title">Sign-in</h2>
+
+                    <div class="input-field">
+                        <i class="fa-solid fa-user"></i>
+                        <input type="text" placeholder="Username">
+                    </div>
+
+                    <div class="input-field">
+                        <i class="fa-solid fa-key"></i>
+                        <input type="password" placeholder="Password">
+                    </div>
+
+                    <input type="submit" value="Login" class="btn solid">
+
+                    <p class="social-text">Or Sign in with social platforms</p>
+
+                    <div class="social-media">
+                        <a href="#" class="social-icon">
+                            <i class="fa-brands fa-facebook"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fa-brands fa-twitter"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fa-brands fa-google"></i>
+                        </a>
+                        <a href="#" class="social-icon">
+                            <i class="fa-brands fa-linkedin-in"></i>
+                        </a>
+                    </div>
+                </form>
+            </div>
+        </div>
+
+        <div class="panels-container">
+
+        </div>
+    </div>
+
+    <?php
+
+    ?>
+    
+</body>
+</html>
