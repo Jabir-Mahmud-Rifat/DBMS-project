a8f5d8717e0af4f8914817890bce6a572811ed8b
S.M Jabir Mahmud <117467591+Jabir-Mahmud-Rifat@users.noreply.github.com>
2022-12-25 11:18:06 +0600

    update

--- signup.php
+++ signup.php
@@ -1,10 +1,14 @@
 <?php
-include ("connection.php");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: access");
 header("Access-Control-Allow-Methods: POST");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
+
+
+include ("connection.php");
+
 
   echo "sign up page is working";
    if(isset($_POST['submit'])) {
@@ -14,7 +18,8 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
      
 
 
-     $query = "INSERT INTO  sign_up (User_name,email,paswd )  VALUES ('$username',' $email','$password')";
+     $query = "INSERT INTO  sign_up (User_name,email,paswd )
+       VALUES ('$username',' $email','$password')";
 
       $run = mysqli_query ($conn, $query);
 
