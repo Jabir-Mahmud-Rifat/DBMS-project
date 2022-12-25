b717060ad67b992c55b3be4ae39f15e7c2294b8d
S.M Jabir Mahmud <117467591+Jabir-Mahmud-Rifat@users.noreply.github.com>
2022-12-25 11:11:13 +0600

    update

--- signup.php
+++ signup.php
@@ -1,5 +1,11 @@
 <?php
 include ("connection.php");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: access");
+header("Access-Control-Allow-Methods: POST");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+
   echo "sign up page is working";
    if(isset($_POST['submit'])) {
      $username = $_POST ['username '];
