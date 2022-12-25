39045f5cd575bac1f59f38939383a29de9dd7558
Arjun <arjunkarmaker03@gmail.com>
2022-12-25 23:16:45 +0600

    landing page first look
    designs created

--- index.php
+++ index.php
@@ -15,51 +15,62 @@
     <link rel="stylesheet" href="css\style.css">
 </head>
 <body>
-    <header class="header" id="header">
-        <nav class="nav container">
-            <a href="index.php" class="nav-logo">
-                <i class="fa-solid fa-vest-patches"></i> Retail Club
-            </a>
+    <div class="self">
+        <header class="header" id="header">
+            <nav class="nav container">
+                <a href="index.php" class="nav-logo">
+                    <i class="fa-solid fa-vest-patches"></i> Retail Club
+                </a>
 
-            <div class="nav-menu" id="nav-menu">
+                <div class="nav-menu" id="nav-menu">
                 <ul class="nav-list">
-                    <li class="nav-item">
-                        <a href="index.php" class="nav-link active-link">Home</a>
-                    </li>
+                        <li class="nav-item">
+                            <a href="index.php" class="nav-link active">Home</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Shop</a>
-                    </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Shop</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">Cart</a>
-                    </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">Cart</a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a href="#" class="nav-link">About</a>
-                    </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link">About</a>
+                        </li>
 
-                    <li class="nav-item">
+                        <li class="nav-item">
                         <a href="#" class="nav-link">Contact</a>
-                    </li>
-                </ul>
-            </div>
+                        </li>
+                    </ul>
+                </div>
 
-            <div class="nav-btn">
+                <div class="nav-btn">
 
-                <ul class="nav-list">
-                    <li class="nav-item">
-                        <a href="index.php" class="login-btn" id="login-btn"><i class="fa-solid fa-user"></i></a>
-                    </li>
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a href="index.php" class="login-btn" id="login-btn"><i class="fa-solid fa-user"></i></a>
+                        </li>
 
-                    <li class="nav-item">
-                        <a href="#" class="nav-cart" id="cart-shop""><i class="fa-solid fa-cart-shopping"></i></a>
-                    </li>
-                </ul>
+                        <li class="nav-item">
+                            <a href="#" class="nav-cart" id="cart-shop""><i class="fa-solid fa-cart-shopping"></i></a>
+                        </li>
+                    </ul>
 
-            </div>
-        </nav>
-    </header>
+                </div>
+            </nav>
+        </header>
+
+        <section class="hero">
+            <h4>ABOVE THE CLOUDS</h4>
+            <h2>Wear your <span>best</span> <br> version</h2>
+            <h6>From waste to wear, our innovative clothing features recycled fabrics</h6>
+            <p>Save more with coupons & upto 50% off</p>
+            <button>Shop now</button>
+
+        </section>
+    </div>
     
 </body>
 </html>
