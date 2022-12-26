d010932a9b0bc4910dc4c85cdf1350d6edcca60b
S.M Jabir Mahmud <117467591+Jabir-Mahmud-Rifat@users.noreply.github.com>
2022-12-26 10:19:30 +0600

    database connection

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
 
   // database connection 
-  $conn = mysqli_connect('localhost','root','','supper_shop');
+  $conn = mysqli_connect('localhost','root','','new_supershop');
 
   if(!$conn){
     echo 'plese check your database ';
